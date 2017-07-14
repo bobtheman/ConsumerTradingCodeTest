0bdbbf61ad32a62b476ea276e8a70d266fe876cc
bobtheman <rob_owen@outlook.com>
2017-07-14 12:21:40 +0100

    Add files via upload

---
+++ ConsumerTrading/ConsumerForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ConsumerTrading
+{
+    partial class ConsumerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCSV = new System.Windows.Forms.TextBox();
+            this.txtXml = new System.Windows.Forms.TextBox();
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.lblCSVName = new System.Windows.Forms.Label();
+            this.txterror = new System.Windows.Forms.TextBox();
+            this.btnExportXml = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnupload = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCSV
+            // 
+            this.txtCSV.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.txtCSV.Location = new System.Drawing.Point(27, 37);
+            this.txtCSV.Name = "txtCSV";
+            this.txtCSV.Size = new System.Drawing.Size(441, 20);
+            this.txtCSV.TabIndex = 0;
+            // 
+            // txtXml
+            // 
+            this.txtXml.Location = new System.Drawing.Point(27, 125);
+            this.txtXml.Multiline = true;
+            this.txtXml.Name = "txtXml";
+            this.txtXml.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtXml.Size = new System.Drawing.Size(522, 219);
+            this.txtXml.TabIndex = 1;
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Location = new System.Drawing.Point(24, 109);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(105, 13);
+            this.lbltitle.TabIndex = 2;
+            this.lbltitle.Text = "XML Output Preview";
+            // 
+            // lblCSVName
+            // 
+            this.lblCSVName.AutoSize = true;
+            this.lblCSVName.Location = new System.Drawing.Point(24, 21);
+            this.lblCSVName.Name = "lblCSVName";
+            this.lblCSVName.Size = new System.Drawing.Size(71, 13);
+            this.lblCSVName.TabIndex = 3;
+            this.lblCSVName.Text = "CSV Input file";
+            // 
+            // txterror
+            // 
+            this.txterror.Location = new System.Drawing.Point(27, 350);
+            this.txterror.Multiline = true;
+            this.txterror.Name = "txterror";
+            this.txterror.Size = new System.Drawing.Size(522, 62);
+            this.txterror.TabIndex = 5;
+            this.txterror.Visible = false;
+            // 
+            // btnExportXml
+            // 
+            this.btnExportXml.Location = new System.Drawing.Point(27, 72);
+            this.btnExportXml.Name = "btnExportXml";
+            this.btnExportXml.Size = new System.Drawing.Size(522, 23);
+            this.btnExportXml.TabIndex = 2;
+            this.btnExportXml.Text = "Process CSV to XML";
+            this.btnExportXml.UseVisualStyleBackColor = true;
+            this.btnExportXml.Click += new System.EventHandler(this.btnExportXml_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnupload
+            // 
+            this.btnupload.Location = new System.Drawing.Point(474, 37);
+            this.btnupload.Name = "btnupload";
+            this.btnupload.Size = new System.Drawing.Size(75, 20);
+            this.btnupload.TabIndex = 1;
+            this.btnupload.Text = "Upload CSV";
+            this.btnupload.UseVisualStyleBackColor = true;
+            this.btnupload.Click += new System.EventHandler(this.btnupload_Click);
+            // 
+            // ConsumerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(570, 421);
+            this.Controls.Add(this.btnupload);
+            this.Controls.Add(this.btnExportXml);
+            this.Controls.Add(this.txterror);
+            this.Controls.Add(this.lblCSVName);
+            this.Controls.Add(this.lbltitle);
+            this.Controls.Add(this.txtXml);
+            this.Controls.Add(this.txtCSV);
+            this.Name = "ConsumerForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Consumer Trading CSV to XML";
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCSV;
+        private System.Windows.Forms.TextBox txtXml;
+        private System.Windows.Forms.Label lbltitle;
+        private System.Windows.Forms.Label lblCSVName;
+        private System.Windows.Forms.TextBox txterror;
+        private System.Windows.Forms.Button btnExportXml;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnupload;
+    }
+}
+
---
+++ ConsumerTrading/ConsumerForm.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using DataStreams.Csv;
+using Microsoft.VisualBasic.FileIO;
+using System.Data.OleDb;
+
+namespace ConsumerTrading
+{
+    public partial class ConsumerForm : Form
+    {
+        public ConsumerForm()
+        {
+            InitializeComponent();
+            txterror.Visible = false;
+        }
+
+        private void LoadFromFile(string filepath,string filename)
+        {
+            try
+            {
+                if (File.Exists(filepath))
+                {
+                    using (TextFieldParser csvParser = new TextFieldParser(filepath))
+                    {
+                        csvParser.CommentTokens = new string[] { "#" };
+                        csvParser.SetDelimiters(new string[] { "," });
+                        csvParser.HasFieldsEnclosedInQuotes = true;
+
+                        // Skip the row with the column names
+                        csvParser.ReadLine();
+
+                        DateTime management_fee_date = DateTime.Parse("01/01/1990").Date;
+
+                        DataTable dt = new DataTable { TableName = "PolicyDetails" };
+                        dt.Columns.Add("policy_number");
+                        dt.Columns.Add("policy_start_date");
+                        dt.Columns.Add("premiums");
+                        dt.Columns.Add("membership");
+                        dt.Columns.Add("management_fee");
+                        dt.Columns.Add("discretionary_bonus");
+                        dt.Columns.Add("uplift_percentage");
+                        dt.Columns.Add("policy_type");
+                        dt.Columns.Add("maturity");
+
+                        while (!csvParser.EndOfData)
+                        {
+                            string[] fields = csvParser.ReadFields();
+                            string policy_number = fields[0];
+                            DateTime policy_start_date = DateTime.Parse(fields[1]).Date;
+                            double premiums = Convert.ToDouble(fields[2]);
+                            string membership = fields[3];
+                            double discretionary_bonus = Convert.ToDouble(fields[4]);
+                            double uplift_percentage = Convert.ToDouble(fields[5]);
+                            string policy_type = policy_number[0].ToString();
+
+                            double management_fee = 0;
+                            double result = 1 + (uplift_percentage / 100);
+                            double uplift_percentage_value = ((uplift_percentage / 100) * 10);
+
+                            if (policy_type == "A")
+                            {
+                                management_fee = Convert.ToDouble("0.03");
+                            }
+                            else if (policy_type == "B")
+                            {
+                                management_fee = Convert.ToDouble("0.05");
+                            }
+                            else
+                            {
+                                management_fee = Convert.ToDouble("0.07");
+                            }
+
+                            double maturity = (premiums - (premiums * management_fee) + discretionary_bonus) * result;
+
+                            dt.Rows.Add(new object[] { policy_number, policy_start_date, premiums, membership, management_fee, discretionary_bonus, uplift_percentage, policy_type, Math.Round(maturity, 2) });
+                        }
+
+                        dt.WriteXml(filename);
+                        StringWriter sw = new StringWriter();
+                        dt.WriteXml(sw);
+                        txtXml.Text = sw.ToString();
+                    }
+                }
+                else
+                {
+                    txterror.Visible = true;
+                    txterror.Text = "Please select a vaild file";
+                }
+
+                MessageBox.Show("XML file has been generated");
+            }
+            catch (Exception ex)
+            {
+                txterror.Visible = true;
+                txterror.Text = ex.Message;
+            }
+        }
+
+        private void btnExportXml_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "XML Files (*.xml)|*.xml"; //sets the file type to xml
+            sfd.FileName = "MaturityData";
+            string sfdname = sfd.FileName;
+            if (sfd.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                LoadFromFile(txtCSV.Text, Path.GetFullPath(sfd.FileName));
+            }
+        }
+
+        private void btnupload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "Text files | *.csv"; // file types, that will be allowed to upload
+                dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+                if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+                {
+                    String filepath = dialog.FileName; // get name of file
+                    using (StreamReader reader = new StreamReader(new FileStream(filepath, FileMode.Open)))
+                    {
+                        txtCSV.Text = filepath;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                txterror.Visible = true;
+                txterror.Text = ex.Message;
+            }
+            
+        }
+    }
+}
